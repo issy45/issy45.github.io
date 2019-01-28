d90d10d05a1cbb32c72faa793f2f531d17436c28
ishino <shungo.ishino@gmail.com>
2019-01-29 01:09:16 +0900

    script修正

--- realtime-translation/script.js
+++ realtime-translation/script.js
@@ -1,38 +1,38 @@
-$(document).ready(function(){
-  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+$(document).ready(function () {
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-  var recognition = new SpeechRecognition();
-  var grammar = '#JSGF V1.0; grammar phrase;';
-  var speechRecognitionList = new SpeechGrammarList();
+  var recognition = new SpeechRecognition()
+  var grammar = '#JSGF V1.0; grammar phrase;'
+  var speechRecognitionList = new SpeechGrammarList()
 
-  var startBtn = document.querySelector('button.start');
-  var stopBtn = document.querySelector('button.stop');
+  var startBtn = document.querySelector('button.start')
+  var stopBtn = document.querySelector('button.stop')
 
   var source = 'ja'
   var target = 'en'
-  $('.source').change(function() {
+  $('.source').change(function () {
     source = this.value
     if (source == 'ja') {
-      recognition.lang = 'ja-JP';
+      recognition.lang = 'ja-JP'
     } else if (source == 'en') {
-      recognition.lang = 'en-US';
+      recognition.lang = 'en-US'
     } else if (source == 'zh') {
-      recognition.lang = 'zh-CN';
+      recognition.lang = 'zh-CN'
     }
-  });
-  $('.target').change(function() {
+  })
+  $('.target').change(function () {
     target = this.value
-  });
+  })
 
-  speechRecognitionList.addFromString(grammar, 1);
-  recognition.grammars = speechRecognitionList;
-  recognition.continuous = true;
-  recognition.interimResults = true;
-  recognition.maxAlternatives = 1;
+  speechRecognitionList.addFromString(grammar, 1)
+  recognition.grammars = speechRecognitionList
+  recognition.continuous = true
+  recognition.interimResults = true
+  recognition.maxAlternatives = 1
 
-  recognition.onresult = function(event) {
+  recognition.onresult = function (event) {
     let res_ja = event.results[event.results.length - 1][0].transcript
     if (event.results[event.results.length - 1]['isFinal']) {
       let url = 'https://script.google.com/macros/s/AKfycbwBGFYiGV7m4AAT-V8mQB6_MvJ3dLcwL7y6vmmp1bpBxxqqYws/exec'
@@ -52,64 +52,20 @@ $(document).ready(function(){
     }
   }
 
-  recognition.onspeechend = function() {
-    startBtn.disabled = false;
+  recognition.onspeechend = function () {
+    startBtn.disabled = false
   }
 
-  recognition.onerror = function(event) {
-    startBtn.disabled = false;
+  recognition.onerror = function (event) {
+    startBtn.disabled = false
   }
 
-  recognition.onaudiostart = function(event) {
-      //Fired when the user agent has started to capture audio.
-      console.log('SpeechRecognition.onaudiostart');
-  }
-
-  recognition.onaudioend = function(event) {
-      //Fired when the user agent has finished capturing audio.
-      console.log('SpeechRecognition.onaudioend');
-  }
-
-  recognition.onend = function(event) {
-      //Fired when the speech recognition service has disconnected.
-      console.log('SpeechRecognition.onend');
-  }
-
-  recognition.onnomatch = function(event) {
-      //Fired when the speech recognition service returns a final result with no significant recognition. This may involve some degree of recognition, which doesn't meet or exceed the confidence threshold.
-      console.log('SpeechRecognition.onnomatch');
-  }
-
-  recognition.onsoundstart = function(event) {
-      //Fired when any sound — recognisable speech or not — has been detected.
-      console.log('SpeechRecognition.onsoundstart');
-  }
-
-  recognition.onsoundend = function(event) {
-      //Fired when any sound — recognisable speech or not — has stopped being detected.
-      console.log('SpeechRecognition.onsoundend');
-  }
-
-  recognition.onspeechstart = function (event) {
-      //Fired when sound that is recognised by the speech recognition service as speech has been detected.
-      console.log('SpeechRecognition.onspeechstart');
-  }
-  recognition.onstart = function(event) {
-      //Fired when the speech recognition service has begun listening to incoming audio with intent to recognize grammars associated with the current SpeechRecognition.
-      console.log('SpeechRecognition.onstart');
-  }
-
-  function startSpeech() {
-    startBtn.disabled = true;
-    recognition.start();
-  }
-
-  function stopSpeech() {
-    startBtn.disabled = false;
-
-    recognition.stop();
-  }
-
-  startBtn.addEventListener('click', startSpeech);
-  stopBtn.addEventListener('click', stopSpeech);
-});
+  startBtn.addEventListener('click', () => {
+    startBtn.disabled = true
+    recognition.start()
+  })
+  stopBtn.addEventListener('click', () => {
+    startBtn.disabled = false
+    recognition.stop()
+  })
+})
