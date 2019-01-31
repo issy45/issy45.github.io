f263d297663e24e03eaea3b2d35a7a6d97499c6e
ishino <shungo.ishino@gmail.com>
2019-02-01 00:48:11 +0900

    け

--- webrtc/script.js
+++ webrtc/script.js
@@ -1,86 +1,24 @@
-/* eslint-disable require-jsdoc */
 $(function() {
 
-  // SpeechRecognition
-  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+  let localStream
 
-  let recognition = new SpeechRecognition()
-  const grammar = '#JSGF V1.0; grammar phrase;'
-  let speechRecognitionList = new SpeechGrammarList()
-
-  speechRecognitionList.addFromString(grammar, 1)
-  recognition.grammars = speechRecognitionList
-  recognition.continuous = true
-  recognition.interimResults = true
-  recognition.maxAlternatives = 1
-
-  recognition.onresult = function (event) {
-    let res_ja = event.results[event.results.length - 1][0].transcript
-    if (event.results[event.results.length - 1]['isFinal']) {
-      $('.temp-text').text('')
-      $('.res-ja').append(res_ja + '。<br>')
-    } else {
-      $('.temp-text').text(res_ja)
-    }
-  }
-
-  recognition.onend = function(event) {
-    recognition.start()
-  }
-
-  // Peer object
   const peer = new Peer({
     key: '81539926-513a-4816-ae7c-9b110b376543',
     debug: 3,
-  });
-
-  let localStream;
-  let existingCall;
+  })
 
   peer.on('open', () => {
-    $('#my-id').text(peer.id);
-    step1();
-  });
-
-  // Receiving a call
-  peer.on('call', call => {
-    // Answer the call automatically (instead of prompting user) for demo purposes
-    call.answer(localStream);
-    step3(call);
-  });
+    step1()
+  })
 
   peer.on('error', err => {
     alert(err.message);
-    // Return to step 2 if error occurs
-    step2();
-  });
+    step2()
+  })
 
-  $('#make-call').on('submit', e => {
-    e.preventDefault();
-    // Initiate a call!
-    console.log($('#callto-id').val());
-    const call = peer.call($('#callto-id').val(), localStream);
-    step3(call);
-  });
-
-  $('#end-call').on('click', () => {
-    existingCall.close();
-    step2();
-  });
-
-  // Retry if getUserMedia fails
-  $('#step1-retry').on('click', () => {
-    $('#step1-error').hide();
-    step1();
-  });
-
-  // set up audio and video input selectors
   const audioSelect = $('#audioSource');
   const videoSelect = $('#videoSource');
   const selectors = [audioSelect, videoSelect];
-
   navigator.mediaDevices.enumerateDevices()
     .then(deviceInfos => {
       const values = selectors.map(select => select.val() || '');
@@ -118,6 +56,47 @@ $(function() {
       audioSelect.on('change', step1);
     });
 
+  const roomName = location.pathname
+  if (!roomName) {
+    return
+  }
+  let room = peer.joinRoom('sfu_video_' + roomName, {mode: 'sfu', stream: localStream});
+  room.on('open', function() {
+    connect(room);
+  });
+  step3(room);
+
+  // SpeechRecognition
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+  var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+  var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+  let recognition = new SpeechRecognition()
+  const grammar = '#JSGF V1.0; grammar phrase;'
+  let speechRecognitionList = new SpeechGrammarList()
+
+  speechRecognitionList.addFromString(grammar, 1)
+  recognition.grammars = speechRecognitionList
+  recognition.continuous = true
+  recognition.interimResults = true
+  recognition.maxAlternatives = 1
+
+  recognition.onresult = function (event) {
+    let res_ja = event.results[event.results.length - 1][0].transcript
+    if (event.results[event.results.length - 1]['isFinal']) {
+      $('.temp-text').text('')
+      $('.res-ja').append(res_ja + '。<br>')
+      room.send(res_ja);
+    } else {
+      $('.temp-text').text(res_ja)
+    }
+  }
+
+  recognition.onend = function(event) {
+    recognition.start()
+  }
+
+
   function step1() {
     // Get audio/video stream
     const audioSource = $('#audioSource').val();
@@ -131,8 +110,8 @@ $(function() {
       $('#my-video').get(0).srcObject = stream;
       localStream = stream;
 
-      if (existingCall) {
-        existingCall.replaceStream(stream);
+      if (room) {
+        room.replaceStream(stream);
         return;
       }
 
@@ -144,31 +123,81 @@ $(function() {
   }
 
   function step2() {
+    $('#their-videos').empty();
     $('#step1, #step3').hide();
     $('#step2').show();
-    $('#callto-id').focus();
+    $('#join-room').focus();
     recognition.stop()
   }
 
-  function step3(call) {
-    // Hang up on an existing call if present
-    if (existingCall) {
-      existingCall.close();
-    }
+  function step3(room) {
     // Wait for stream on the call, then set peer video display
-    call.on('stream', stream => {
-      const el = $('#their-video').get(0);
+    room.on('stream', stream => {
+      const peerId = stream.peerId;
+      const id = 'video_' + peerId + '_' + stream.id.replace('{', '').replace('}', '');
+
+      $('#their-videos').append($(
+        '<div class="video_' + peerId +'" id="' + id + '">' +
+          '<label>' + stream.peerId + ':' + stream.id + '</label>' +
+          '<video class="remoteVideos" autoplay playsinline>' +
+        '</div>'));
+      const el = $('#' + id).find('video').get(0);
       el.srcObject = stream;
       el.play();
     });
 
-    recognition.start()
+    room.on('removeStream', stream => {
+      const peerId = stream.peerId;
+      $('#video_' + peerId + '_' + stream.id.replace('{', '').replace('}', '')).remove();
+    });
 
     // UI stuff
-    existingCall = call;
-    $('#their-id').text(call.remoteId);
-    call.on('close', step2);
+    room.on('close', step2);
+    room.on('peerLeave', peerId => {
+      $('.video_' + peerId).remove();
+    });
     $('#step1, #step2').hide();
     $('#step3').show();
+
+    recognition.start()
+  }
+
+  function connect(room) {
+    // Handle a chat connection.
+    room.once('log', logs => {
+      for (let i = 0; i < logs.length; i++) {
+        const log = JSON.parse(logs[i]);
+
+        switch (log.messageType) {
+          case 'ROOM_DATA':
+            $('.res-ja').append('<div><span class="peer">' + log.message.src + '</span>: ' + log.message.data + '</div>');
+            break;
+          case 'ROOM_USER_JOIN':
+            if (log.message.src === peer.id) {
+              break;
+            }
+            $('.res-ja').append('<div><span class="peer">' + log.message.src + '</span>: has joined the room </div>');
+            break;
+          case 'ROOM_USER_LEAVE':
+            if (log.message.src === peer.id) {
+              break;
+            }
+            $('.res-ja').append('<div><span class="peer">' + log.message.src + '</span>: has left the room </div>');
+            break;
+        }
+      }
+    });
+
+    room.on('data', message => {
+      if (message.data instanceof ArrayBuffer) {
+        const dataView = new Uint8Array(message.data);
+        const dataBlob = new Blob([dataView]);
+        const url = URL.createObjectURL(dataBlob);
+        $('.res-ja').append('<div><span class="file">' +
+          message.src + ' has sent you a <a target="_blank" href="' + url + '">file</a>.</span></div>');
+      } else {
+        $('.res-ja').append('<div><span class="peer">' + message.src + '</span>: ' + message.data + '</div>');
+      }
+    });
   }
 });
